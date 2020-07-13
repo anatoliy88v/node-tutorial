f773b2d298e5c8d8b6b6d01057f73a1801fe6dd7
Anatoliy88v <anatoliy88v@gmail.com>
2020-07-13 10:25:20 +0300

    added editing

--- models/course.js
+++ models/course.js
@@ -38,6 +38,27 @@ class Course {
     })
   }
 
+  static async update(course) {
+    const courses = await Course.getAll()
+
+    const idx = courses.findIndex(c => c.id === course.id)
+    courses[idx] = course
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        }
+      )
+    })
+  }
+
   static getAll() {
     return new Promise((resolve, reject) => {
       fs.readFile(
@@ -52,6 +73,11 @@ class Course {
         }
       )
     })
+  }
+
+  static async getById(id) {
+    const courses = await Course.getAll()
+    return courses.find(c => c.id === id)
   }
 }
 
