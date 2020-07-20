f5b29eaadb7de017a52531b17c9ae2675294325a
Anatoliy88v <anatoliy88v@gmail.com>
2020-07-20 09:59:19 +0300

    added cart logic

--- models/card.js
+++ models/card.js
@@ -11,7 +11,7 @@ class Card {
   static async add(course) {
     const card = await Card.fetch()
 
-    const idx = card.courses.findIndex(c => c.id === courseid)
+    const idx = card.courses.findIndex(c => c.id === course.id)
     const candidate = card.courses[idx]
 
     if (candidate) {
@@ -32,6 +32,33 @@ class Card {
           reject(err)
         } else {
           resolve()
+        }
+      })
+    })
+  }
+
+  static async remove(id) {
+    const card = await Card.fetch()
+
+    const idx = card.courses.findIndex(c => c.id === id)
+    const course = card.courses[idx]
+
+    if (course.count === 1) {
+      // delete
+      card.courses = card.courses.filter(c => c.id !== id)
+    } else {
+      // change quantity
+      card.courses[idx].count--
+    }
+
+    card.price -= course.price
+
+    return new Promise ((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(card), err => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(card)
         }
       })
     })
